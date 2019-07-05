181577bb25891dd7b047ed11aae411e96ad195e2
HassannR <34104827+HassannR@users.noreply.github.com>
2019-07-05 23:07:52 +0200

    Updated comic book detail view

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,6 +10,17 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
+           ViewBag.SeriesTitle = "The Amazing Spider-Man";
+           ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doc Oc's life and his one, last,great, act of revenge! Even ifSpider-Man survives... <strong>will peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+                {
+                    "Script: Dann Slott",
+                    "Pencils: Hamada",
+                    "Colors: Ronaldo",
+                    "Letters: Hassan Ragab"
+
+                };
 
             return View();
            
